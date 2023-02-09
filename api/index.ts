a5e21a3a4b8d1951c01cce2a9e13eac66050b0aa
Alfredo Blanco <alfre.blanco@icloud.com>
2023-02-09 11:51:49 -0300

    Ready to deploy

--- api/index.ts
+++ api/index.ts
@@ -1,19 +1,14 @@
 const app = require('./src/app');
-const socketio = require('socket.io');
+const { Server } = require('socket.io');
 const http = require('http');
 const mongoosed = require('mongoose');
 require('dotenv').config();
 const { PORT } = process.env;
 
 const connection = mongoosed.connection;
-/* 
-    mongod --dbpath /data/db --replSet "rs0" --port 27017
-    mongod --dbpath /data/db1 --replSet "rs0" --port 27018
-    mongod --dbpath /data/db2 --replSet "rs0" --port 27019
-    mongo rs.initiate({_id : "rs0", members: [{_id : 0, host : "localhost:27017"},{_id : 1, host : "localhost:27018"},{_id : 2, host : "localhost:27019"}]})
-*/
+
 const server = http.createServer(app);
-const io = socketio(server, {
+const io = new Server(server, {
     cors: {
         origin : "*",
         methods : ['GET', 'POST']
