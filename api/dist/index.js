8fafeb14e10eda77732558987be36cba702aaec7
Alfredo Blanco <alfre.blanco@icloud.com>
2023-02-09 16:29:31 -0300

    Fixing CORS

--- api/dist/index.js
+++ api/dist/index.js
@@ -6,16 +6,10 @@ const mongoosed = require('mongoose');
 require('dotenv').config();
 const { PORT } = process.env;
 const connection = mongoosed.connection;
-/*
-    mongod --dbpath /data/db --replSet "rs0" --port 27017
-    mongod --dbpath /data/db1 --replSet "rs0" --port 27018
-    mongod --dbpath /data/db2 --replSet "rs0" --port 27019
-    mongo rs.initiate({_id : "rs0", members: [{_id : 0, host : "localhost:27017"},{_id : 1, host : "localhost:27018"},{_id : 2, host : "localhost:27019"}]})
-*/
 const server = http.createServer(app);
 const io = socketio(server, {
     cors: {
-        origin: "*",
+        origin: process.env.CLIENT_URL,
         methods: ['GET', 'POST']
     }
 });
