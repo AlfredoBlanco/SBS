4d5d19c102ab18fd88c74cae00b55719d26e4950
Alfredo Blanco <alfre.blanco@icloud.com>
2023-02-16 13:54:01 -0300

    Try

--- web-a/src/socket.ts
+++ web-a/src/socket.ts
@@ -1,7 +1,5 @@
 import io from 'socket.io-client';
 
-let socket = io(`${process.env.NEXT_PUBLIC_API_PATH}/`,{
-    rejectUnauthorized: false
-});
+let socket = io(`${process.env.NEXT_PUBLIC_API_PATH}/`);
 
 export default socket;
