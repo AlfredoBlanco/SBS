4521b1916bfda4d43f82826650ee8919a2c11294
Alfredo Blanco <alfre.blanco@icloud.com>
2023-02-22 18:51:07 -0300

    Try

--- web-a/src/socket.ts
+++ web-a/src/socket.ts
@@ -2,6 +2,7 @@ import io from 'socket.io-client';
 
 let socket = io(`${process.env.NEXT_PUBLIC_API_PATH}/`,{
     transports: ['websocket'],
+    withCredentials: true,
 });
 
 export default socket;
